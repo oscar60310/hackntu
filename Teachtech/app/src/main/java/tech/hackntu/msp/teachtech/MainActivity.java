0917fa0c03a128ed43f67b337198138d28a53d79
Tsai chen ping <oscar60310@gmail.com>
2016-08-20 02:01:41 +0800

    list view 1 dataset

--- Teachtech/app/src/main/java/tech/hackntu/msp/teachtech/MainActivity.java
+++ Teachtech/app/src/main/java/tech/hackntu/msp/teachtech/MainActivity.java
@@ -1,10 +1,14 @@
 package tech.hackntu.msp.teachtech;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,11 +18,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import data.callback;
 import data.web;
@@ -28,6 +43,9 @@ public class MainActivity extends AppCompatActivity
 
 
     public Activity mac;
+    private Intent intent;
+    private JSONObject count_data;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -54,36 +72,107 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        ListView listView = (ListView) findViewById(R.id.app_list);
-        //找到ListView
+        ((EditText)findViewById(R.id.search_field)).addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        String[] values = new String[]{
-                "Line",
-                "Facebook",
-                "Twitter",
-                "LinkedIn"
-        };
-        //丟入你要顯示的文字
+            }
 
-        ListAdapter adapter = new ArrayAdapter<>(this , android.R.layout.simple_list_item_1 ,values);
-        //使用ListAdapter來顯示你輸入的文字
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-        listView.setAdapter(adapter);
-        //將ListAdapter設定至ListView裡面
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                update_list(((EditText)findViewById(R.id.search_field)).getText()+"", count_data);
+            }
+        });
 
         //發送GET查詢APP和數量
         final web webget = new web(new callback.AsyncResponse() {
             @Override
             public void processFinish(JSONObject output) {
                 //回傳資料
-
-                new AlertDialog.Builder(mac).setMessage(output.toString()).show();
+                count_data = output;
+                update_list("",output);
+                //new AlertDialog.Builder(mac).setMessage(output.toString()).show();
 
             }
         }
         );
+        // init listview
+        count_list = new ArrayList<>();
+        adapter = new SimpleAdapter(this, count_list, R.layout.list_items,
+                new String[] { "app", "count" }, new int[] {
+                R.id.app, R.id.count });
+        ListView lv = (ListView) findViewById(R.id.app_list);
+        lv.setAdapter(adapter);
+
         webget.execute("http://goofydog.me/jellyfish/hack/count_sample.json");
     }
+    //當使用者輸入搜尋，篩選資料
+    void update_list(String filter,JSONObject data)
+    {
+
+        count_list.clear();
+        if(filter == "")
+        {
+            //No filter
+            try
+            {
+                JSONArray datas = (JSONArray)data.get("data");
+
+                for(int i=0;i<datas.length();i++)
+                {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("app", ((JSONObject)datas.get(i)).get("app"));
+                    map.put("count", ((JSONObject)datas.get(i)).get("count"));
+                    count_list.add(map);
+                }
+            }
+            catch (Exception e)
+            {
+                Log.d("e",e.toString());
+            }
+
+        }
+        else
+        {
+            //全部轉成大寫比較
+
+             filter = filter.toUpperCase();
+            try
+            {
+                JSONArray datas = (JSONArray)data.get("data");
+
+                for(int i=0;i<datas.length();i++)
+                {
+                    String app =  ((JSONObject)datas.get(i)).get("app").toString().toUpperCase();
+                    if(app.startsWith(filter))
+                    {
+                        Map<String, Object> map = new HashMap<>();
+                        map.put("app", ((JSONObject)datas.get(i)).get("app"));
+                        map.put("count", ((JSONObject)datas.get(i)).get("count"));
+                        count_list.add(map);
+
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Log.d("e",e.toString());
+            }
+        }
+
+        /// 給資料更新LIST
+        Log.d("list",count_list.size()+"");
+        adapter.notifyDataSetChanged();
+    }
+    private List<Map<String, Object>> count_list;
+    private SimpleAdapter adapter;
+
+
     public interface AsyncResponse{void processFinish(JSONObject output);}
     @Override
     public void onBackPressed() {
